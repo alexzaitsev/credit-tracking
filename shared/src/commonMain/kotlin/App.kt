e49492f6b85310a11a04a38d053218fba02fff54
Alex Zaitsev <alex.zaitsev.pro@gmail.com>
2023-04-23 11:53:30 -0600

    first commit

---
+++ shared/src/commonMain/kotlin/App.kt
@@ -0,0 +1,123 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.lazy.items
+
+@Composable
+fun App() {
+    MaterialTheme {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Row(modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)) {
+                Name(modifier = Modifier.weight(1f), name = "Oksana")
+                Name(modifier = Modifier.weight(1f), name = "Alex")
+            }
+            Row {
+                Column(modifier = Modifier.weight(1f)) {
+                    Account(
+                        modifier = Modifier.weight(1f)
+                            .background(color = Color.Gray, shape = RoundedCornerShape(5.dp)),
+                        bankName = "CIBC"
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Account(
+                        modifier = Modifier.weight(1f).background(
+                            color = Color.LightGray,
+                            shape = RoundedCornerShape(5.dp)
+                        ),
+                        bankName = "Servus"
+                    )
+                }
+                Spacer(modifier = Modifier.width(20.dp))
+                Column(modifier = Modifier.weight(1f)) {
+                    Account(
+                        modifier = Modifier.weight(1f).background(
+                            color = Color.LightGray,
+                            shape = RoundedCornerShape(5.dp)
+                        ),
+                        bankName = "CIBC"
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Account(
+                        modifier = Modifier.weight(1f)
+                            .background(color = Color.Gray, shape = RoundedCornerShape(5.dp)),
+                        bankName = "Servus"
+                    )
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+private fun Name(modifier: Modifier, name: String) = Text(
+    modifier = modifier,
+    textAlign = TextAlign.Center,
+    fontWeight = FontWeight.Bold,
+    fontSize = 22.sp,
+    text = name
+)
+
+@Composable
+private fun Account(modifier: Modifier, bankName: String, transactions: List<Transaction>) =
+    Column(modifier = modifier) {
+        Text(
+            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 20.sp,
+            text = bankName
+        )
+        Status(transactions)
+        Transactions(transactions)
+        Spacer(modifier = Modifier.weight(1f))
+        AddBtn(modifier = Modifier.align(Alignment.CenterHorizontally))
+    }
+
+@Composable
+private fun Transactions(transactions: List<Transaction>) {
+    LazyColumn {
+        items(transactions) {
+
+        }
+    }
+}
+
+@Composable
+private fun Status(transactions: List<Transaction>) = Column(
+    modifier = Modifier.fillMaxWidth()
+        .background(color = Color.Green, shape = RoundedCornerShape(5.dp))
+        .padding(8.dp)
+) {
+    Text("No issues found")
+}
+
+@Composable
+private fun AddBtn(modifier: Modifier) = Button(
+    modifier = modifier,
+    onClick = {
+        // TODO navigate
+    },
+    content = {
+        Text("Add transaction")
+    })
+
+expect fun getPlatformName(): String
---
+++ shared/src/commonMain/kotlin/Transaction.kt
@@ -0,0 +1,6 @@
+data class Transaction(
+    val id: Long,
+    val dateTime: Long,
+    val amount: Float,
+    val comment: String
+)
